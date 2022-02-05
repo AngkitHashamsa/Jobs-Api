34ad1de398044d41edd1616dc2cd8929be1197cf
Angkit <angkithashamsa21@gmail.com>
2022-02-06 02:05:07 +0530

    Ready for heroku

--- app.js
+++ app.js
@@ -4,16 +4,37 @@ const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
 const auth = require("./middleware/authentication");
+
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
+const cors = require("cors");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+const limiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
+// app.use(rateLimiter);
 app.use(express.json());
 // extra packages
-
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // routes
+app.use('/',(req,res)=>{
+ res.status(200).send('Jobs Api')
+})
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", auth, jobsRouter);
 // app.get("/", (req, res) => {
